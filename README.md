0c6cea83428676e78c39b07572bef32dafae3919
zanllp <zanllp@163.com>
2021-01-13 22:49:09 +0800

    feat: 增加负数和比较器乘除的支持

--- README.md
+++ README.md
@@ -1,15 +1,62 @@
 # TypeFP
 类型体操实践，将typescript的type部分拿来当函数式语言使用。欢迎pr补充
 ## 基本数字操作
+## 定义，拆包
 ```ts
-type int = Int<20> // 声明一个不可变的int
-type int2 = Plus<int,Int<22>> // 等同于 Int<42>
-type num = Unpack<int2> // expect 42 拆包，返回一个字面量42
-type int3 =  Subtract<int2,Int<2>> // expect Int<40>
+type int = Int<20> // 定义一个不可变的int
+type num = Unpack<int> // expect 20 拆包，返回一个数字字面量20
+
+type int2 = Int<-1> // 定义一个负数
+type num2 = Unpack<int2> // expect -1 拆包，返回一个字面量-1
+```
+## 比较
+```ts
+type comp = Compare<Int<0>, Int<0>> // expect true
+type comp1 = Compare<Int<0>, Int<-1>> // expect false
+
+type comp2 = LessThan<Int<0>, Int<0>> // expect false
+type comp3 = LessThan<Int<0>, Int<-1>> // expect false
+type comp4 = LessThan<Int<-1>, Int<-3>> // expect false
+type comp5 = LessThan<Int<-2>, Int<20>> // expect true
+type comp6 = LessThan<Int<-2>, Int<-1>> // expect true
+```
+## 四则运算
+```ts
+type int = Int<1>
+// 加
+type plus1 = Plus<int> // +1
+type res1 = Unpack<plus1> // 2
+type plus2 = Plus<int,Int<10>>
+type res2 = Unpack<plus2> // 11
+type plus3 = Plus<int,Int<-10>>
+type res3 = Unpack<plus3> // -9
+
+// 减
+type subtract1 = Subtract<int> // -1
+type res4 = Unpack<subtract1> // 0
+type subtract2 = Subtract<int,Int<10>>
+type res5 = Unpacksubtract2> // -9
+type subtract3 =Subtract<int,Int<-10>>
+type res6 = Unpack<subtract3> // 11
+
+// 乘
+type res7 = Unpack<Multiply<int, Int<10>>> // expect 10
+type res8 = Unpack<Multiply<int, Int<-5>>> // expect -5
+
+// 除
+type res9 = Unpack<Divide<int, Int<10>>> // expect 0
+type res10 = Unpack<Divide<int, Int<0>>> // expect never
+type res11 = Unpack<Divide<Int<5>, int>> // expect 5
+type res12 = Unpack<Divide<Int<5>, Int<2>>> // expect 2
+type res13 = Unpack<Divide<Int<5>, Int<-2>>> // expect -2
+
+// 混合
+type mix = Multiply<Int<2>, Plus<Divide<Int<5>, Int<2>>>>
+type res14 = Unpack<mix> // expect 6
 ```
 ## 数组操作
 ```ts
-type arr = [1, 2, 3, 'hello', 'world'] // 声明一个不可变数组
+type arr = [1, 2, 3, 'hello', 'world'] // 定义一个不可变数组
 type arr1 = Shift<arr> // expect [2,3,'hello','world'] 去掉数组最前面的元素，并返回一个新数组
 type lastItem = Last<arr> // 返回数组最后一个元素，expect 'world'
 type len = Len<arr1> // expect 4
--- src/index.d.ts
+++ src/index.d.ts
@@ -1,56 +1,160 @@
-type _Base = any[]
+type BaseType<T = any> = T[]
 
 /**
  * 删除数组最前面（头部）的元素，并返回这个数组
  */
-export type Shift<T extends _Base> = 
-    T extends [infer A, ...infer B] 
-        ? B 
+export type Shift<T extends BaseType> =
+    T extends [infer A, ...infer B]
+        ? B
         : never
 
 /**
  * 返回数组最后一个数
  */
-export type Last<T extends _Base> =
+export type Last<T extends BaseType> =
     T['length'] extends 0
         ? undefined
         : T['length'] extends 1
             ? T[0]
             : Last<Shift<T>>
 
+export type Absolute<T extends number> =
+    `${T}` extends `-${infer neg}`
+        ? ToNumber<neg>
+        : T
+
+export type ToNumber<S extends string, T extends BaseType = []> =
+    S extends `${T["length"]}`
+        ? T["length"]
+        : ToNumber<S, [...T, any]>
+
+export type LessThanZero<T extends number> = `${T}` extends `-${infer neg}` ? true : false
+
 /**
  * 生成一个INT
  * @param T 初始化数
  */
-export type Int<T extends number, C extends _Base = []> =
-    C['length'] extends T
-        ? C
-        : Int<T, [...C, undefined]>
+export type Int<T extends number, C extends BaseType = [], P extends boolean = true> =
+    LessThanZero<T> extends true
+        ? Int<Absolute<T>, C, false>
+        : C['length'] extends T
+            ? [C, P]
+            : Int<T, [...C, null], P>
 
-export type Len<T extends _Base> = T['length']
+type IntType = [BaseType, boolean]
+type UintType = [BaseType, true]
+
+export type Len<T extends BaseType> = T['length']
 
 /**
- * 解包，把Int转成数字字面量
+ * 解包，把Int转成数字字面量,负数是字符串字面量
  */
-export type Unpack<T extends _Base> = Len<T>
+export type Unpack<T extends IntType> = T[1] extends true ? Len<T[0]> : `-${Len<T[0]>}`
 
 /**
  * []的alias
  */
-export type Index<T extends _Base, I extends number> = T[I]
+export type Index<T extends BaseType, I extends number> = T[I]
+
+
+export type Compare<A, B> = A extends B ? true : false
+
+/**
+ * 无符号比较
+ * A 小于 B返回true
+ */
+export type LessThanU<A extends UintType, B extends UintType> = 
+    A extends Int<0>
+        ? B extends Int<0>
+            ? false // 相等
+            : true // B 大于 A
+        : B extends Int<0>
+            ? false
+            : LessThanU<[Shift<A[0]>, true], [Shift<B[0]>, true]>
+            
+/**
+ * 比较
+ * A 小于 B返回true
+ */
+export type LessThan<A extends IntType, B extends IntType> = 
+    A[1] extends true
+        ? B[1] extends true
+            ? LessThanU<[A[0], true], [B[0], true]>
+            : false
+        : B[1] extends true
+            ? true
+            : LessThanU<[B[0], true], [A[0], true]>
+
 
 /**
  * 对一个int进行加法，返回一个新的int
  */
-export type Plus<Lhs extends _Base, Rhs extends _Base = Int<1>> = [...Lhs, ...Rhs]
+export type Plus<A extends IntType, B extends IntType = Int<1>> =
+    A[1] extends true
+        ? B[1] extends true
+            ? [[...A[0], ...B[0]], true] // a + b
+            : Subtract<A, [B[0], true]> // a + -b = a - b
+        : B[1] extends true
+            ? Subtract<B, [A[0], true]> // -a + b = b - a
+            : [[...A[0], ...B[0]], false] // -a + -b = -(1+1)
 
 /**
  * 减法
  */
-export type Subtract<Lhs extends _Base, Rhs extends _Base = Int<1>> =
-    Rhs extends Int<0>
-        ? Lhs
-        : Subtract<Shift<Lhs>, Shift<Rhs>>
+export type Subtract<A extends IntType, B extends IntType = Int<1>> =
+    A[1] extends true
+        ? B[1] extends true
+            ? B extends Int<0> // a - b, B先到0
+                ? A
+                : A extends Int<0> // a 先到0
+                    ? [B[0], false]
+                    : Subtract<[Shift<A[0]>, true], [Shift<B[0]>, true]>
+            : Plus<A, [B[0], true]> // a - -b = a + b
+        : B[1] extends true
+            ? [[...A[0], ...B[0]], false] // -a - b = - (a+b)
+            : Subtract<[B[0], true], [A[0], true]> // -a - -b = b-a
+
+/**
+ * 乘法
+ * @param A，B  乘号左右
+ * @param R 每次递归的结果
+ * @param I 深度
+ */
+export type Multiply<A extends IntType, B extends IntType, R extends IntType = Int<0>, L extends IntType = Int<0>> =
+    B[1] extends false 
+        ? A[1] extends true // b 小于 0
+            ? Multiply<[A[0], false],[B[0], true]> // A大于0
+            : Multiply<[A[0], true],[B[0], true]> // A小于0
+        : L extends B
+            ? R
+            : Multiply<A, B, Plus<A, R>, Plus<L>>
+
+
+/**
+ * 无符号除法
+ * @param A，B  左右
+ * @param R 每次递归的结果
+ * @param I 深度
+ */
+export type DivideU<A extends UintType, B extends UintType, R extends IntType = Int<0>, L extends IntType = Int<0>> = 
+    B extends Int<0>
+        ? never
+        : LessThan<A, R> extends true 
+            ? Subtract<L>
+            : DivideU<A, B, Plus<R, B>, Plus<L>>
+/**
+ * 除法
+ * A 小于 B返回true
+ */
+export type Divide<A extends IntType, B extends IntType> = 
+    A[1] extends true
+        ? B[1] extends true
+            ? DivideU<[A[0], true], [B[0], true]>
+            : [[...DivideU<[A[0], true], [B[0], true]>][0], false]
+        : B[1] extends true
+            ? [[...DivideU<[A[0], true], [B[0], true]>][0], false]
+            :  DivideU<[A[0], true], [B[0], true]>
+        
 
 /**
  * 将长度限制
@@ -59,20 +163,21 @@ export type Subtract<Lhs extends _Base, Rhs extends _Base = Int<1>> =
  * @param R 每次递归的结果
  * @param I 深度
  */
-export type MaxLen<T extends _Base, L extends _Base = Int<0>, R extends _Base = [], I extends _Base = Int<0>> =
-    L extends Int<0>
+export type MaxLen<T extends BaseType, L extends IntType = Int<-1>, R extends BaseType = [], I extends IntType = Int<0>> =
+    L extends Int<-1>
         ? T
         : I extends L
-            ? R 
-            : MaxLen<T, L, [...R, T[Unpack<I>]], Plus<I>>
+            ? R
+            : MaxLen<T, L, [...R, T[Unpack<[I[0], true]>]], Plus<I>>
 
+
+            
 /**
  * @param T  数组
  * @param S 开始位置
  * @param L 长度，默认截到末尾
  */
-export type Slice<T extends _Base, S extends _Base, L extends _Base = Int<0>> =
+export type Slice<T extends BaseType, S extends IntType, L extends IntType = Int<-1>> =
     S extends Int<0>
         ? MaxLen<T, L>
         : Slice<Shift<T>, Subtract<S>, L>
-        
